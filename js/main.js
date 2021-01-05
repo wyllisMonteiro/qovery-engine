4e33a8df9a437cbe76a46ada0adbf1a282307281
wyllisMonteiro <wyllismonteiro@gmail.com>
2021-01-05 22:06:14 +0100

    feat: add aos effect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-const highlight = require("./modules/highlight")
+const AOS           = require("aos")
+const highlight     = require("./modules/highlight")
 
 window.addEventListener("DOMContentLoaded", (event) => {
+    AOS.init()
     highlight.rust()
 });
